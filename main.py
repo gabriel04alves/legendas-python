43dddff7a42e35dcb5f813553161501e0acd3629
Gabriel Alves <gabriel52.alves@gmail.com>
2025-04-07 21:18:55 -0300

    feat: refactor caption generation and improve text functionality

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-name_file = "video.mp4"
-file_location = f"/app/{name_file}"
-type_file = name_file.split(".")[1]
+file_name = "video.mp4"
+file_location = f"/app/{file_name}"
+type_file = file_name.split(".")[1]
 
 
 audio_file = AudioSegment.from_file(file_location, format=type_file)
@@ -17,11 +17,29 @@ audio_file.export("/app/audio.mp3", format="mp3")
 model = WhisperModel("medium", compute_type="int8")
 
 segments, _ = model.transcribe("/app/audio.mp3", language="pt")
+segments_list = list(segments)
 
-translated = " ".join([segment.text for segment in segments])
+
+def format_srt_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = seconds % 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:06.3f}".replace(".", ",")
 
 
-def improve_text_translated(text):
+def create_caption_srt(segments, file_name_caption="legendas.srt"):
+    with open(file_name_caption, "w", encoding="utf-8") as file:
+        for i, segment in enumerate(segments, 1):
+            start = format_srt_time(segment.start)
+            end = format_srt_time(segment.end)
+            text = segment.text.strip()
+
+            file.write(f"{i}\n")
+            file.write(f"{start} --> {end}\n")
+            file.write(f"{text}\n\n")
+
+
+def improve_text(text):
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
     headers = {
@@ -51,14 +69,20 @@ def improve_text_translated(text):
     response = requests.post(
         "https://api.groq.com/openai/v1/chat/completions", headers=headers, json=body
     )
-    result = response.json()
-    return result["choices"][0]["message"]["content"]
+
+    return response.json()["choices"][0]["message"]["content"]
 
 
-print("Texto original transcrito:")
-print(translated)
+caption_text = "\n".join(
+    [
+        f"{format_srt_time(s.start)} --> {format_srt_time(s.end)}\n{s.text}"
+        for s in segments_list
+    ]
+)
 
-print("-------")
+improved_caption = improve_text(caption_text)
 
-print("\nTexto após melhoria com Groq:")
-print(improve_text_translated(translated))
+with open("/app/legendas.srt", "w", encoding="utf-8") as file:
+    file.write(improved_caption)
+
+print(f"- Foi gerado o arquivo legendas.srt")
