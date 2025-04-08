eaa623b7bdae67db6cc1190eb037ba63846d043f
Gabriel Alves <gabriel52.alves@gmail.com>
2025-04-08 17:29:49 -0300

    feat: add apply_subtitle function to integrate subtitle application in the video processing pipeline

---
+++ app/apply_subtitle.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def apply_subtitle(video_path, subtitle_path, output_path):
+    command = [
+        "ffmpeg",
+        "-i",
+        video_path,
+        "-vf",
+        f"subtitles={subtitle_path}:force_style='Fontsize=18'",
+        "-c:a",
+        "copy",
+        output_path,
+    ]
+
+    subprocess.run(command, check=True)
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from app.transcription import transcribe_audio
 from app.text_improve import improve_text
 from app.format_srt_time import format_srt_time
 from app.srt_create import create_srt
+from app.apply_subtitle import apply_subtitle
 
 INPUT_VIDEO = "/app/video.mp4"
 EXTRACTED_AUDIO = "/app/audio.mp3"
 OUTPUT_SRT = "/app/legendas.srt"
+OUTPUT_VIDEO = "/app/video_com_legenda.mp4"
 
 extract_audio(INPUT_VIDEO, EXTRACTED_AUDIO)
 segments = list(transcribe_audio(EXTRACTED_AUDIO))
@@ -16,4 +18,6 @@ for s in segments:
 
 create_srt(segments, OUTPUT_SRT)
 
-print("✔ Legendas geradas com sucesso!")
+apply_subtitle(INPUT_VIDEO, OUTPUT_SRT, OUTPUT_VIDEO)
+
+print("Legendas geradas e aplicadas com sucesso!")
